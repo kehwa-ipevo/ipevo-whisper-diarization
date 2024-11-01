a3ec5dbb41fadb8a3e9161dbed5b43e628f4b277
kehwaweng <kehwaweng@staff.ipevo.com>
2024-11-01 15:10:57 +0800

    update build script

--- build_exe.sh
+++ build_exe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 設置環境變數
-CONDA_ENV_PATH="/home/ipevox0244/anaconda3/envs/diarization"
+CONDA_ENV_PATH="/home/ipevox0244/anaconda3/envs/test"
 PYTHON_LIB_PATH="$CONDA_ENV_PATH/lib/python3.10/site-packages"
 PYTHON_LIB_DYNLOAD="$CONDA_ENV_PATH/lib/python3.10/lib-dynload"
 
@@ -11,12 +11,12 @@ pyinstaller \
     --add-data "$PYTHON_LIB_PATH/pytorch_lightning/version.info:./pytorch_lightning" \
     --add-data "$PYTHON_LIB_PATH/lightning_fabric/version.info:./lightning_fabric" \
     --add-data "$PYTHON_LIB_PATH/ctc_forced_aligner/punctuations.lst:ctc_forced_aligner" \
-    --add-data "$PYTHON_LIB_PATH/whisperx/assets:whisperx/assets" \
     --add-data "$PYTHON_LIB_PATH/ctc_forced_aligner/uroman:uroman" \
     --add-data "$PYTHON_LIB_PATH/ctc_forced_aligner/uroman/bin/uroman.pl:uroman" \
     --add-data "$PYTHON_LIB_PATH/ctc_forced_aligner/uroman/lib/NLP/Chinese.pm:uroman/lib/NLP" \
     --add-binary "$PYTHON_LIB_DYNLOAD/_sqlite3.cpython-310-x86_64-linux-gnu.so:." \
     --add-data "$PYTHON_LIB_PATH/faster_whisper/assets/silero_encoder_v5.onnx:faster_whisper/assets" \
+    --add-data "$PYTHON_LIB_PATH/faster_whisper/assets/silero_decoder_v5.onnx:faster_whisper/assets" \
     --collect-all torch \
     --collect-all torchvision \
     --collect-all rich \
