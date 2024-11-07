d51d8ac2ea1e2bb4f1465050b738453aecc5350d
kehwaweng <kehwaweng@staff.ipevo.com>
2024-11-07 15:16:55 +0800

    fix runtime error, add chunk size as argument

--- diarize.py
+++ diarize.py
@@ -231,3 +231,104 @@ def main(args: argparse.Namespace):
         write_srt(ssm, srt)
 
     cleanup(temp_path)
+
+
+def get_args() -> argparse.Namespace:
+    """get arguments
+
+    Returns:
+        argparse.Namespace : parsed arguments
+    """    
+
+    # Initialize parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-a", 
+        "--audio", 
+        help="name of the target audio file", 
+        required=True
+    )
+    parser.add_argument(
+        "--no-stem",
+        action="store_false",
+        dest="stemming",
+        default=True,
+        help="Disables source separation."
+        "This helps with long files that don't contain a lot of music.",
+    )
+
+    parser.add_argument(
+        "--suppress_numerals",
+        action="store_true",
+        dest="suppress_numerals",
+        default=False,
+        help="Suppresses Numerical Digits."
+        "This helps the diarization accuracy but converts all digits into written text.",
+    )
+
+    parser.add_argument(
+        "--whisper-model",
+        dest="model_name",
+        default="medium.en",
+        help="name of the Whisper model to use",
+    )
+
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        dest="batch_size",
+        default=8,
+        help="Batch size for batched inference, reduce if you run out of memory, "
+        "set to 0 for original whisper longform inference",
+    )
+
+    parser.add_argument(
+        "--language",
+        type=str,
+        default=None,
+        choices=whisper_langs,
+        help="Language spoken in the audio, specify None to perform language detection",
+    )
+
+    parser.add_argument(
+        "--device",
+        dest="device",
+        default="cuda" if torch.cuda.is_available() else "cpu",
+        help="if you have a GPU use 'cuda', otherwise 'cpu'",
+    )
+
+    parser.add_argument(
+        "--check-update",
+        action="store_true",
+        help="Check for updates app update",
+    )
+
+    parser.add_argument(
+        "--version",
+        action="version",
+        help="show current app version"
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    ## get args
+    args = get_args()
+
+    ## if version is on, show version and exit
+    if args.version:
+        from updater import get_current_version
+        print(f"current version: {get_current_version()}")
+        exit(0)
+
+    ## if update is on, update app and exit
+    if args.check_update:
+        from updater import update
+        update()
+        exit(0)
+
+    ## main process
+    main(args)
