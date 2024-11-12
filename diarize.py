461f5109a27e5b8376ae5e35df745924a6c992da
kehwaweng <kehwaweng@staff.ipevo.com>
2024-11-12 10:21:55 +0800

    diarize add measure time decorator

--- diarize.py
+++ diarize.py
@@ -35,6 +35,21 @@ from helpers import (
 
 mtypes = {"cpu": "int8", "cuda": "float16"}
 
+from functools import wraps
+import time
+
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
+        return result
+    return timeit_wrapper
+
+@timeit
 def main(args: argparse.Namespace):
     """whisper diarization main process
 
