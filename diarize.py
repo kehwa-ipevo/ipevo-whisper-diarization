5ca4f7d1453937eddcf9ebadf27b02efecf46617
MahmoudAshraf97 <hassouna97.ma@gmail.com>
2024-11-14 18:06:21 +0200

    update code to work with `faster-whisper` latest

--- diarize.py
+++ diarize.py
@@ -134,10 +134,114 @@ def main(args: argparse.Namespace):
     del alignment_model
     torch.cuda.empty_cache()
 
+<<<<<<< HEAD
     tokens_starred, text_starred = preprocess_text(
         full_transcript,
         romanize=True,
         language=langs_to_iso[info.language],
+=======
+# clear gpu vram
+del whisper_model, whisper_pipeline
+torch.cuda.empty_cache()
+
+# Forced Alignment
+alignment_model, alignment_tokenizer = load_alignment_model(
+    args.device,
+    dtype=torch.float16 if args.device == "cuda" else torch.float32,
+)
+
+emissions, stride = generate_emissions(
+    alignment_model,
+    torch.from_numpy(audio_waveform)
+    .to(alignment_model.dtype)
+    .to(alignment_model.device),
+    batch_size=args.batch_size,
+)
+
+del alignment_model
+torch.cuda.empty_cache()
+
+tokens_starred, text_starred = preprocess_text(
+    full_transcript,
+    romanize=True,
+    language=langs_to_iso[info.language],
+)
+
+segments, scores, blank_token = get_alignments(
+    emissions,
+    tokens_starred,
+    alignment_tokenizer,
+)
+
+spans = get_spans(tokens_starred, segments, blank_token)
+
+word_timestamps = postprocess_results(text_starred, spans, stride, scores)
+
+
+# convert audio to mono for NeMo combatibility
+ROOT = os.getcwd()
+temp_path = os.path.join(ROOT, "temp_outputs")
+os.makedirs(temp_path, exist_ok=True)
+torchaudio.save(
+    os.path.join(temp_path, "mono_file.wav"),
+    torch.from_numpy(audio_waveform).unsqueeze(0).float(),
+    16000,
+    channels_first=True,
+)
+
+
+# Initialize NeMo MSDD diarization model
+msdd_model = NeuralDiarizer(cfg=create_config(temp_path)).to(args.device)
+msdd_model.diarize()
+
+del msdd_model
+torch.cuda.empty_cache()
+
+# Reading timestamps <> Speaker Labels mapping
+
+
+speaker_ts = []
+with open(os.path.join(temp_path, "pred_rttms", "mono_file.rttm"), "r") as f:
+    lines = f.readlines()
+    for line in lines:
+        line_list = line.split(" ")
+        s = int(float(line_list[5]) * 1000)
+        e = s + int(float(line_list[8]) * 1000)
+        speaker_ts.append([s, e, int(line_list[11].split("_")[-1])])
+
+wsm = get_words_speaker_mapping(word_timestamps, speaker_ts, "start")
+
+if info.language in punct_model_langs:
+    # restoring punctuation in the transcript to help realign the sentences
+    punct_model = PunctuationModel(model="kredor/punctuate-all")
+
+    words_list = list(map(lambda x: x["word"], wsm))
+
+    labled_words = punct_model.predict(words_list, chunk_size=230)
+
+    ending_puncts = ".?!"
+    model_puncts = ".,;:!?"
+
+    # We don't want to punctuate U.S.A. with a period. Right?
+    is_acronym = lambda x: re.fullmatch(r"\b(?:[a-zA-Z]\.){2,}", x)
+
+    for word_dict, labeled_tuple in zip(wsm, labled_words):
+        word = word_dict["word"]
+        if (
+            word
+            and labeled_tuple[1] in ending_puncts
+            and (word[-1] not in model_puncts or is_acronym(word))
+        ):
+            word += labeled_tuple[1]
+            if word.endswith(".."):
+                word = word.rstrip(".")
+            word_dict["word"] = word
+
+else:
+    logging.warning(
+        f"Punctuation restoration is not available for {info.language} language."
+        " Using the original punctuation."
+>>>>>>> accf5c0 (update code to work with `faster-whisper` latest)
     )
 
     segments, scores, blank_token = get_alignments(
